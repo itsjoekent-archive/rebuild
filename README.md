599eaa0bfae3f4e4e4394977687f9c8898026fed
Joe Kent <897368+itsjoekent@users.noreply.github.com>
2019-01-06 15:12:33 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# rebuild
+
+_Rebuild the single-page app CI/CD experience!_
+
+Set of Github actions to test & deploy SPA's to S3, update Cloudflare K/V storage and post Slack deploy messages. Also contains an AWS Lambda function for retrieving metadata of a deployment.
+
+**rebuild** as a project is only concerned with creating a new immutable static deployment in cloud object storage for every commit. To point your deployments at a domain or subdomain, use [to be named](...).
+
+# rebuild slack commands
+
+**rebuild** contains an AWS Lambda function that is invoked by the following Slack command, `/rebuild <deployment-link>`
+
+This function is deployed to AWS by [deploy-lambda-action](https://github.com/lannonbr/deploy-lambda-action).
+
+# rebuild github actions
+
+Drop these Github actions into any SPA repositories workflow to get started.
+
+## run-tests
+
+Checks if the attached repository has a `ci:test` command defined in its `package.json`. Updates the pull request with test results and posts a summary to Slack.
+
+```
+Workflow TODO
+```
+
+The following credentials are required,
+
+- TODO...
+
+## ship-it
+
+Creates a "deployment" for every commit for staging and production environments. Deployments are identified by the repository name, commit hash and environment. Links to deployments for both environments are placed in the pull request and posted to Slack.
+
+```
+Workflow TODO
+```
+
+The following credentials are required,
+
+- TODO...
+
+Additionally, any secrets with the following naming prefixes will be written to an `.env` file before the build process for each environment.
+
+- `REBUILD_${YOUR_KEY_HERE}`. Applies to both both staging & production builds.
+- `REBUILD_STAGING_${YOUR_KEY_HERE}`. Applies to staging builds only.
+- `REBUILD_PRODUCTION_${YOUR_KEY_HERE}`. Applies to production builds only.
+
+Due to Github actions, there is a cap of 100 secrets.
---
+++ run-tests/index.js
@@ -0,0 +1,94 @@
+const { promises: fsp } = require('fs');
+const path = require('path');
+const childProcess = require('child_process');
+
+const { Toolkit } = require('actions-toolkit');
+const tools = new Toolkit();
+const octokit = tools.createOctokit();
+
+const GITHUB_WORKSPACE = process.env.GITHUB_WORKSPACE;
+const TEST_WORKSPACE = path.join(process.cwd(), '/rebuild-test');
+
+const TEST_STATUS_IN_PROGRESS = 'in_progress';
+const TEST_STATUS_COMPLETED = 'completed';
+
+const TEST_CONCLUSION_SUCCESS = 'success';
+const TEST_CONCLUSION_FAILURE = 'failure';
+
+async function copyDir() {
+  const entries = await fsp.readdir(GITHUB_WORKSPACE, { withFileTypes: true });
+  await fsp.mkdir(TEST_WORKSPACE);
+
+  for (let entry of entries) {
+    const srcPath = path.join(GITHUB_WORKSPACE, entry.name);
+    const destPath = path.join(TEST_WORKSPACE, entry.name);
+
+    if (entry.isDirectory()) {
+      await copyDir(srcPath, destPath);
+    } else {
+      await fsp.copyFile(srcPath, destPath);
+    }
+  }
+}
+
+async function installModules() {
+  childProcess.execSync('npm install', {
+    cwd: TEST_WORKSPACE,
+  });
+}
+
+async function runTests() {
+  const package = require(path.join(TEST_WORKSPACE, 'package.json'));
+  const { scripts } = package;
+
+  if (! scripts || ! scripts['ci:test']) {
+    return;
+  }
+
+  childProcess.execSync(scripts['ci:test'], {
+    cwd: TEST_WORKSPACE,
+  });
+}
+
+async function updateGithubStatus(status, conclusion = null, message = null) {
+  const params = {
+    ...tools.context.repo(),
+    ...tools.context.issue(),
+    status,
+    head_sha: tools.context.sha,
+    name: 'rebuild tests',
+  };
+
+  if (status === TEST_STATUS_IN_PROGRESS) {
+    params.started_at = new Date().toISOString();
+  }
+
+  if (conclusion) {
+    params.conclusion = conclusion;
+    params.completed_at = new Date().toISOString();
+
+    if (message) {
+      params.output = {
+        title: 'rebuild tests',
+        summary: conclusion === TEST_CONCLUSION_SUCCESS ? 'Success' : 'Failure',
+        text: message,
+      };
+    }
+  }
+
+  await octokit.checks.create(params);
+}
+
+new Promise((resolve, reject) => {
+  await updateGithubStatus(TEST_STATUS_IN_PROGRESS);
+  resolve();
+})
+  .then(copyDir)
+  .then(installModules)
+  .then(runTests)
+  .then(() => await updateGithubStatus(TEST_STATUS_COMPLETED, TEST_CONCLUSION_SUCCESS))
+  .catch(async (error) => {
+    const message = `${error.message}\n${error.stack}`;
+
+    await updateGithubStatus(TEST_STATUS_COMPLETED, TEST_CONCLUSION_FAILURE, message);
+  });
