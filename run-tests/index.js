e9c8ca947b65a5212bc1a5f29219386087a51a9b
Joe Kent <897368+itsjoekent@users.noreply.github.com>
2019-01-06 16:07:01 -0500

    Copying things is hard

--- run-tests/index.js
+++ run-tests/index.js
@@ -7,7 +7,6 @@ const tools = new Toolkit();
 const octokit = tools.createOctokit();
 
 const GITHUB_WORKSPACE = process.env.GITHUB_WORKSPACE;
-const TEST_WORKSPACE = '/usr/src/rebuild-test'
 
 const TEST_STATUS_IN_PROGRESS = 'in_progress';
 const TEST_STATUS_COMPLETED = 'completed';
@@ -17,34 +16,18 @@ const TEST_CONCLUSION_FAILURE = 'failure';
 
 let checkId = null;
 
-async function copyDir(srcDir, destDir) {
-  const entries = await fsp.readdir(srcDir, { withFileTypes: true });
-  await fsp.mkdir(destDir);
-
-  for (let entry of entries) {
-    const srcPath = path.join(srcDir, entry.name);
-    const destPath = path.join(destDir, entry.name);
-
-    if (entry.isDirectory()) {
-      await copyDir(srcPath, destPath);
-    } else {
-      await fsp.copyFile(srcPath, destPath);
-    }
-  }
-}
-
 async function installModules() {
   console.log('Installing modules');
 
   childProcess.execSync('npm install', {
-    cwd: TEST_WORKSPACE,
+    cwd: GITHUB_WORKSPACE,
   });
 }
 
 async function runTests() {
   console.log('Running tests');
 
-  const package = require(path.join(TEST_WORKSPACE, 'package.json'));
+  const package = require(path.join(GITHUB_WORKSPACE, 'package.json'));
   const { scripts } = package;
 
   if (! scripts || ! scripts['ci:test']) {
@@ -52,7 +35,7 @@ async function runTests() {
   }
 
   childProcess.execSync(scripts['ci:test'], {
-    cwd: TEST_WORKSPACE,
+    cwd: GITHUB_WORKSPACE,
   });
 }
 
@@ -96,10 +79,6 @@ async function updateGithubStatus(status, conclusion = null, message = null) {
 new Promise((resolve, reject) => {
   updateGithubStatus(TEST_STATUS_IN_PROGRESS).then(resolve);
 })
-  .then(() => {
-    console.log('Copying files to test folder');
-    return copyDir(GITHUB_WORKSPACE, TEST_WORKSPACE);
-  })
   .then(installModules)
   .then(runTests)
   .then(() => updateGithubStatus(TEST_STATUS_COMPLETED, TEST_CONCLUSION_SUCCESS))
